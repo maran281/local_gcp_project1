5f0a8e15e2625204f1d282b68ebbb7d08657bf41
Manoj Rana (External - HCL Sweden AB) <manoj.rana@ingka.com>
2022-09-02 13:57:42 +0200

    2test1

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "gcs" {
+    bucket = "testblablablabucket"
+    prefix = "pocState"
+    # credentials = "C:/gcp_poc/key/cla-poc-key.json"
+    # credentials = file("./cla-poc-key.json")
+  }
+} 
